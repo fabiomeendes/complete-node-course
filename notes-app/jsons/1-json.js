34972a520d02c65e2a64aa1604f2cd32905f84f6
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-18 16:14:22 -0300

    4. 18. Storing Data with JSON (Challenge)

--- notes-app/jsons/1-json.js
+++ notes-app/jsons/1-json.js
@@ -12,3 +12,13 @@ const fs = require('fs');
 // const dataJSON = dataBuffer.toString();
 // const data = JSON.parse(dataJSON);
 // console.log(data.title);
+
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+
+data.name = 'Fábio'
+data.age = '20'
+
+const dataJSONResult = JSON.stringify(data);
+fs.writeFileSync('1-json.json', dataJSONResult);
