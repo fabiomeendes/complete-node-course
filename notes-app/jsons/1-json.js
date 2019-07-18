2a5889ed840a26ffa0206366929058cb7c5534b6
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-18 16:09:16 -0300

    Create and read a file system - JSON

--- notes-app/jsons/1-json.js
+++ notes-app/jsons/1-json.js
@@ -1,10 +1,14 @@
-const book = {
-  title: 'Harry Potter 11',
-  author: 'Fabio Mendes',
-}
+const fs = require('fs');
 
-const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+// const book = {
+//   title: 'Harry Potter 11',
+//   author: 'Fabio Mendes',
+// }
 
-const parsedData = JSON.parse(bookJSON);
-console.log(parsedData.author);
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
