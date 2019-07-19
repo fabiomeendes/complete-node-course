ef4ecaf5404cdf48be7b6c3c21801cb6030d6ebb
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-19 11:09:54 -0300

    6. 31. Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-  console.log('2 Second Timer')
-}, 2000);
+const url = 'https://api.darksky.net/forecast/20a2f27a213aa1fe82fa41dcfb5ccb3e/37.8267,-122.4233';
 
-setTimeout(() => {
-  console.log('0 Second Timer')
-}, 0);
-
-console.log('Stopping');
+request({ url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.currently);
+});
