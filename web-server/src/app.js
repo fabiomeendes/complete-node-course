db8b6347962dfd437d2434d5a11480a0ba240ff1
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-23 16:18:25 -0300

    7. 45. Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-  res.send('<h1>Weather</h1>');
-});
-
-app.get('/help', (req, res) => {
-  res.send([{
-    name: 'Fabio',
-  },{
-    name: 'Andrew',
-  }]);
-});
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About page!</h1>');
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
   res.send({
