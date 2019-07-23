6216af7aed4e5cda4633372f500bd1f98cdd4f66
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-23 11:47:48 -0300

    6. 37. Callback forecast

---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forecast = (lat, lon, callback) => {
+  const url = `https://api.darksky.net/forecast/20a2f27a213aa1fe82fa41dcfb5ccb3e/${lat},${lon}?units=si&lang=pt`;
+  request({ url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to access the weather service!', undefined);
+    } else if (response.body.error) {
+      callback('Unable to find the location!', undefined);
+    } else {
+      callback(undefined, `${response.body.daily.data[0].summary} - ${response.body.currently.temperature}`);
+    }
+  });
+}
+
+module.exports = forecast;
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -4,9 +4,9 @@ const geocode = (address, callback) => {
   const geoCodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiZmJvNjIxIiwiYSI6ImNqeWE4NWhpYTBjN2EzY2xoYTVobTA3Z20ifQ.XsNUskVE0PUe3EOM8Rj1YA`;
   request({ url: geoCodeURL, json: true }, (error, response) => {
     if (error) {
-      callback('Unable to access the mapbox service!');
+      callback('Unable to access the mapbox service!', undefined);
     } else if (response.body.features.length === 0) {
-      callback('Unable to find the location!');
+      callback('Unable to find the location!', undefined);
     } else {
       callback(undefined, {
         lat: response.body.features[0].center[1],
