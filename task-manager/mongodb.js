24089d99f3c2a7b2ad0e0106f84bd80380a6ab13
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-26 13:49:54 -0300

    10. 80. Updating Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,32 +12,30 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName);
 
-  // db.collection('users').findOne({ _id: new ObjectID("5d3b06f9a8cd9210f4deb95c") }, (error, user) => {
-  //   if (error) {
-  //     return console.log('Unable to get the user');
+  // db.collection('users').updateOne({
+  //   _id: new ObjectID('5d3b05396c3de820941f9f8b'),
+  // }, {
+  //   $set: {
+  //     name: 'Mike1',
+  //   },
+  //   $inc: {
+  //     age: -1
   //   }
-  //   console.log(user)
+  // }).then(result => {
+  //   console.log(result);
+  // }).catch(e => {
+  //   console.log(e);
   // });
 
-  // db.collection('users').find({ age: 20 }).toArray((error, users) => {
-  //   if (error) {
-  //     return console.log('Unable to get the user');
-  //   }
-  //   console.log(users)
-  // });
-
-  // db.collection('users').find({ age: 20 }).count((error, count) => {
-  //   if (error) {
-  //     return console.log('Unable to get the user');
-  //   }
-  //   console.log(count)
-  // });
-
-  db.collection('tasks').findOne({ _id: new ObjectID("5d3b08de8586b04208bd945a") }, (error, task) => {
-    console.log(task._id)
-  });
-
-  db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-    console.log(tasks)
-  });
-})
+  db.collection('tasks').updateMany({
+    completed: false
+  },{
+      $set: {
+        completed: true,
+      },
+    }).then(result => {
+      console.log(result.modifiedCount);
+    }).catch(e => {
+      console.log(e);
+    });
+});
