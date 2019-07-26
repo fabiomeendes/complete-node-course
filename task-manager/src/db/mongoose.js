89cfffaae3e1bbff9a240bbcb611264201829d0b
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-26 14:53:18 -0300

    11. 85. Data Validation and Sanitization: Part I - validator

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,51 +1,69 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
 });
 
-// const User = mongoose.model('User', {
-//   name: {
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Use a positive number');
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
+  }
+});
+
+const me = new User({
+  name: 'Fabio    ',
+  email: '   faf@dDADADAD.com  '
+});
+
+me.save().then(result => {
+  console.log(`${me.name} has been saved!`, me);
+}).catch(e => {
+  console.log(e);
+});
+
+// const Task = mongoose.model('Task', {
+//   description: {
 //     type: String,
 //     required: true
 //   },
-//   age: {
-//     type: Number,
+//   completed: {
+//     type: Boolean,
 //     required: true
 //   }
 // });
 
-// const me = new User({
-//   name: 'Fabio',
-//   age: 20,
+// const task = new Task({
+//   description: 'Learn everything about Node',
+//   completed: false,
 // });
 
-// me.save().then(result => {
-//   console.log(`${me.name} Saved!`);
+// task.save().then(result => {
+//   console.log(task);
 // }).catch(e => {
 //   console.log(e);
 // });
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true
-  },
-  completed: {
-    type: Boolean,
-    required: true
-  }
-});
-
-const task = new Task({
-  description: 'Learn everything about Node',
-  completed: false,
-});
-
-task.save().then(result => {
-  console.log(task);
-}).catch(e => {
-  console.log(e);
-});
 
