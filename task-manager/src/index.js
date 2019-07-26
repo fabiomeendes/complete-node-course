5d6c7510f40ee52953565f1dd32efb982c16688b
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-26 16:19:56 -0300

    11. 90. Resource Creation Endpoints: Part II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose')
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +11,19 @@ app.use(express.json());
 app.post('/users', (req, res) => {
   const user = new User(req.body);
   user.save().then(result => {
-    res.send(user);
+    res.status(201).send(user);
+  }).catch(e => {
+    res.status(400).send({
+      errorMessage: e.message,
+      errorComplete: e
+    });
+  });
+});
+
+app.post('/tasks', (req, res) => {
+  const user = new Task(req.body);
+  user.save().then(result => {
+    res.status(201).send(user);
   }).catch(e => {
     res.status(400).send({
       errorMessage: e.message,
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const User = mongoose.model('User', {
+module.exports = mongoose.model('User', {
   name: {
     type: String,
     required: true,
@@ -39,5 +39,3 @@ const User = mongoose.model('User', {
     }
   }
 });
-
-module.exports = User;
