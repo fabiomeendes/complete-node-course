94d83ca37b9a728cfb6dc4a97d4063723c98134b
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-26 14:31:58 -0300

    11. 84. Creating a Mongoose Model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,24 +5,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
-  name: {
+// const User = mongoose.model('User', {
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   age: {
+//     type: Number,
+//     required: true
+//   }
+// });
+
+// const me = new User({
+//   name: 'Fabio',
+//   age: 20,
+// });
+
+// me.save().then(result => {
+//   console.log(`${me.name} Saved!`);
+// }).catch(e => {
+//   console.log(e);
+// });
+
+const Task = mongoose.model('Task', {
+  description: {
     type: String,
     required: true
   },
-  age: {
-    type: Number,
+  completed: {
+    type: Boolean,
     required: true
   }
 });
 
-const me = new User({
-  name: 'Fabio',
-  age: 20,
+const task = new Task({
+  description: 'Learn everything about Node',
+  completed: false,
 });
 
-me.save().then(result => {
-  console.log(`${me.name} Saved!`);
+task.save().then(result => {
+  console.log(task);
 }).catch(e => {
   console.log(e);
 });
+
