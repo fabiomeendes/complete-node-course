e5313479e4d290962917c46b805bf0506bb3ad5c
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-27 20:04:39 -0300

    11. 96. Async/Await: Part II

--- task-manager/playground/3-promises-chaining-db.js
+++ task-manager/playground/3-promises-chaining-db.js
@@ -1,12 +1,24 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('5d3b40f5266b762714303c4b', { age: 1 }).then(user => {
-  console.log(user);
-  return User.countDocuments({ age: 1 });
-}).then(count => {
-  console.log(count);
-})
-.catch(e => {
-  console.log(e);
+// User.findByIdAndUpdate('5d3b40f5266b762714303c4b', { age: 1 }).then(user => {
+//   console.log(user);
+//   return User.countDocuments({ age: 1 });
+// }).then(count => {
+//   console.log(count);
+// })
+// .catch(e => {
+//   console.log(e);
+// });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount('5d3b4169432b8e2b68b2edaa', 2).then(count => {
+  console.log(count, 'count');
+}).catch(e => {
+  console.log(e, 'e');
 });
--- task-manager/playground/4-promises-chaining-db.js
+++ task-manager/playground/4-promises-chaining-db.js
@@ -1,11 +1,22 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5d3b41eb9299f93f108bb851').then(task => {
-  console.log(task, 'result');
-  return Task.countDocuments({ completed: false });
-}).then(count => {
-  console.log(count, 'count');
+// Task.findByIdAndDelete('5d3b41eb9299f93f108bb851').then(task => {
+//   console.log(task, 'result');
+//   return Task.countDocuments({ completed: false });
+// }).then(count => {
+//   console.log(count, 'count');
+// }).catch(e => {
+//   console.log(e, 'e');
+// });
+
+const deleteTaskAndCount = async (id) => {
+  await Task.findByIdAndDelete(id);
+  return await Task.countDocuments({ completed: false });
+};
+
+deleteTaskAndCount('5d3b51d69a2f1103501f6d61').then(count => {
+  console.log(count);
 }).catch(e => {
-  console.log(e, 'e');
+  console.log(e);
 });
