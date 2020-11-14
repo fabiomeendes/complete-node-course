8416351dbd06b9f06ba3ed3fa8447a52c9c4f845
Fábio Alexandre Mendes Camillo <fabio.camillo@asteria.com.br>
2020-11-14 11:06:23 -0300

    14. 127. Handling Express Errors

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -150,8 +150,11 @@ const upload = multer({
   }
 });
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
-  res.status(200).send();
+// uploadX igual o que deve estar no postman/insomnia
+router.post('/users/me/avatar', upload.single('uploadX'), (req, res) => {
+  res.send();
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message });
 });
 
 module.exports = router;
